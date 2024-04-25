5420cea25b51087273f00a99215f137973c3a186
atomofiron <atomofiron@gmail.com>
2024-04-26 00:43:41 +0200

    up core lib to 1.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
